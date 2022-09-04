6175e0e5c0017b044ba1d68cb380e4a866417c4b
morenicogs <nicolas@grootjans.co>
2022-09-05 01:28:12 +0200

    added control keyboard listeners

---
+++ controls.js
@@ -0,0 +1,43 @@
+class Controls {
+	constructor(){
+		this.forward = false;
+		this.left = false;
+		this.right = false;
+		this.reverse = false;
+
+		this.#addKeyboardListeners();
+	}
+
+	#addKeyboardListeners() {
+		document.onkeydown = (event) => {
+			if(event.key === "ArrowLeft") {
+				this.left = true;
+			}
+			if(event.key === "ArrowRight") {
+				this.right = true;
+			}
+			if(event.key === "ArrowUp") {
+				this.forward = true;
+			}
+			if(event.key === "ArrowDown") {
+				this.reverse = true;
+			}
+			console.dir(this);
+		}
+		document.onkeyup = (event) => {
+			if(event.key === "ArrowLeft") {
+				this.left = false;
+			}
+			if(event.key === "ArrowRight") {
+				this.right = false;
+			}
+			if(event.key === "ArrowUp") {
+				this.forward = false;
+			}
+			if(event.key === "ArrowDown") {
+				this.reverse = false;
+			}
+			console.dir(this);
+		}
+	}
+}
