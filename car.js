5e0ce4e6c0d9ddefe2b4a83ea1530ee09202d5dd
morenicogs <nicolas@grootjans.co>
2022-09-05 04:22:56 +0200

    rotating car added

--- car.js
+++ car.js
@@ -11,6 +11,7 @@ class Car{
 
 		this.acceleration = 0.2;
 		this.friction = 0.05;
+		this.angle = 0;
 
 		this.controls = new Controls();
 	}
@@ -28,33 +29,40 @@ class Car{
 		}
 
 		if(this.controls.left) {
-			this.x -= 2;
+			this.angle += 0.03;
 		}
 
 		if(this.controls.right) {
-			this.x += 2;
+			this.angle -= 0.03;
 		}
 
 		// adding friction
 		if(this.speed) {
 			(this.speed > 0) ? (this.speed -= this.friction) : (this.speed += this.friction);
 		}
-
+		// Set speed to zero at some point 
 		if(Math.abs(this.speed) < this.friction) {
 			this.speed = 0;
 		}
-		this.y -= this.speed;
+
+		this.x -= Math.sin(this.angle) * this.speed;
+		this.y -= Math.cos(this.angle) * this.speed;
 	}
 
 	draw(ctx) {
+		ctx.save();
+		ctx.translate(this.x, this.y);
+		ctx.rotate(-this.angle);
+
 		ctx.beginPath();
 		ctx.rect(
-			this.x - this.width/2,
-			this.y - this.height/2,
+			-this.width/2,
+			-this.height/2,
 			this.width,
 			this.height
 		);
 
 		ctx.fill();
+		ctx.restore();
 	}
 }
