dc75bb71ac9e0c8858d4386f496efe52f93670aa
morenicogs <nicolas@grootjans.co>
2022-09-05 04:57:15 +0200

    reverse angle flipped

--- car.js
+++ car.js
@@ -28,23 +28,28 @@ class Car{
 			this.speed = Math.max(-this.maxSpeed/2, this.speed);
 		}
 
-		if(this.controls.left) {
-			this.angle += 0.03;
-		}
-
-		if(this.controls.right) {
-			this.angle -= 0.03;
-		}
-
-		// adding friction
-		if(this.speed) {
-			(this.speed > 0) ? (this.speed -= this.friction) : (this.speed += this.friction);
-		}
 		// Set speed to zero at some point 
 		if(Math.abs(this.speed) < this.friction) {
 			this.speed = 0;
 		}
+		
 
+		// adding friction
+		if(this.speed) {
+			const flip = (this.speed > 0) ? 1 : -1;
+			
+			(this.speed > 0) ? (this.speed -= this.friction) : (this.speed += this.friction);
+
+			if(this.controls.left) {
+				this.angle += 0.03 * flip;
+			}
+	
+			if(this.controls.right) {
+				this.angle -= 0.03 * flip;
+			}
+
+		}
+		
 		this.x -= Math.sin(this.angle) * this.speed;
 		this.y -= Math.cos(this.angle) * this.speed;
 	}
